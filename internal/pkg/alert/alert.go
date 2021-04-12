fc0318ba2cfd5fcc2de4105e5d6b45d800b1d192
Mike Jones <mike@netsplit.org.uk>
2021-04-12 08:40:48 +0100

    Display whether a post is a self-post or a link-post (closes #20)

--- internal/pkg/alert/alert.go
+++ internal/pkg/alert/alert.go
@@ -6,15 +6,33 @@ import (
 	"github.com/vartanbeno/go-reddit/v2/reddit"
 )
 
+const (
+	postTypeSelf = "Self"
+	postTypeLink = "Link"
+)
+
 // Alerts are pushed to the IRC bot when a new post is made
 type Alert struct {
 	Channels []string
 	Post     *reddit.Post
 }
 
+func (a *Alert) postType() string {
+	var postType string
+
+	if a.Post.IsSelfPost {
+		postType = postTypeSelf
+	} else {
+		postType = postTypeLink
+	}
+
+	return postType
+}
+
 // ToString formats the alert as a string.
 func (a *Alert) ToString() string {
-	alert := fmt.Sprintf(`"%s" posted in /r/%s by %s. %s`, a.Post.Title, a.Post.SubredditName, a.Post.Author, a.Post.URL)
+	prefix := fmt.Sprintf("\x0303%s post:\x03", a.postType())
+	alert := fmt.Sprintf(`%s "%s" posted in /r/%s by %s. %s`, prefix, a.Post.Title, a.Post.SubredditName, a.Post.Author, a.Post.URL)
 
 	if a.Post.NSFW {
 		alert = alert + " \x0304NSFW"
