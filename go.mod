dde5944ac428c35dfb17e8f406cea77ce46e5790
Mike Jones <mike@netsplit.org.uk>
2021-04-12 08:26:15 +0100

    Tests for `Subscriptions.Update()`

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/n7st/subgrok
 go 1.16
 
 require (
+	github.com/davecgh/go-spew v1.1.0
+	github.com/google/go-cmp v0.5.5
 	github.com/shibukawa/configdir v0.0.0-20170330084843-e180dbdc8da0
 	github.com/stretchr/testify v1.7.0
 	github.com/thoj/go-ircevent v0.0.0-20190807115034-8e7ce4b5a1eb
--- internal/pkg/subscription/subscriptions_test.go
+++ internal/pkg/subscription/subscriptions_test.go
@@ -3,10 +3,82 @@ package subscription
 import (
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestUpdate(t *testing.T) {}
+func TestUpdate(t *testing.T) {
+	tests := []struct {
+		name          string
+		want          *Subscriptions
+		subscriptions *Subscriptions
+	}{
+		{
+			name: "one channel, one subreddit",
+			want: &Subscriptions{
+				ChannelToSubreddits: map[string][]string{
+					"##channel": {"first"},
+				},
+				SubredditToChannels: map[string][]string{
+					"first": {"##channel"},
+				},
+				Subreddits: []string{"first"},
+			},
+			subscriptions: &Subscriptions{
+				ChannelToSubreddits: map[string][]string{
+					"##channel": {"first"},
+				},
+			},
+		},
+		{
+			name: "one channel, two subreddits",
+			want: &Subscriptions{
+				ChannelToSubreddits: map[string][]string{
+					"##channel": {"first", "second"},
+				},
+				SubredditToChannels: map[string][]string{
+					"first":  {"##channel"},
+					"second": {"##channel"},
+				},
+				Subreddits: []string{"first", "second"},
+			},
+			subscriptions: &Subscriptions{
+				ChannelToSubreddits: map[string][]string{
+					"##channel": {"first", "second"},
+				},
+			},
+		},
+		{
+			name: "many channels, many subreddits",
+			want: &Subscriptions{
+				ChannelToSubreddits: map[string][]string{
+					"##channel": {"first", "second"},
+					"##other":   {"second", "third"},
+				},
+				SubredditToChannels: map[string][]string{
+					"first":  {"##channel"},
+					"second": {"##channel", "##other"},
+					"third":  {"##other"},
+				},
+				Subreddits: []string{"first", "second", "third"},
+			},
+			subscriptions: &Subscriptions{
+				ChannelToSubreddits: map[string][]string{
+					"##channel": {"first", "second"},
+					"##other":   {"second", "third"},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt.subscriptions.Update()
+
+		if !cmp.Equal(tt.subscriptions, tt.want) {
+			t.Fail()
+		}
+	}
+}
 
 func TestToSubredditString(t *testing.T) {
 	tests := []struct {
